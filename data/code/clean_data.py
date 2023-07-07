95cdd03f0a7d035b64bd3ea0c1d3bd9f416fc9ed
sthau <sapthau@gmail.com>
2023-07-07 11:42:22 -0700

    Change chips_sold histogram to percentages, rebuild paper

--- data/code/clean_data.py
+++ data/code/clean_data.py
@@ -4,16 +4,31 @@ import matplotlib.pyplot as plt
 
 ### DEFINE
 def main():
+    # read data
     df = pd.read_csv('output/data_merged.csv')
-    plot_data(df)
+
+    # clean data
     df = clean_data(df)
+
+    # run plot
+    plot_data(df)
+
+    # save data
     df.to_csv('output/data_cleaned.csv', index = False)
 
 def plot_data(df):
-    plt.hist(df['chips_sold'])
+    # plot histogram of chips_sold variable as percentages
+    plt.hist(df['chips_sold'], weights = np.zeros_like(df['chips_sold']) + 1. / df['chips_sold'].size)
+
+    # add x axis label of chips_sold, y axis of percentage
+    plt.xlabel('chips_sold')
+    plt.ylabel('percentage')
+
+    # save plot to output folder
     plt.savefig('output/chips_sold.pdf')
 
 def clean_data(df):
+    # remove rows with missing indicator
     df['chips_sold'][df['chips_sold'] == -999999] = np.NaN
     return(df)
     
